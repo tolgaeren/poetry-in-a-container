d48848d4d4eb5be0039a813532a45e255edbe8ea
Tolga Eren <tolga.eren_1@philips.com>
2020-07-28 10:06:57 +0200

    entrypoint=entrypoint.sh

---
+++ entrypoint.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# from https://github.com/abatilo/actions-poetry/blob/master/entrypoint.sh 
+set -e
+
+# Push current directory on to stack and cd (if possible) into working dir.
+pushd . > /dev/null 2>&1 || return
+cd "$INPUT_WORKING_DIRECTORY" || return
+
+sh -c "poetry $*"
+
+# Step back to starting directory.
+popd > /dev/null 2>&1 || return
